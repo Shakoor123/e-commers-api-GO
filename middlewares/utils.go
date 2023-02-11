6eec9bca98ad760434a6135cbb67ad03840e3cfd
Noor Mohammed B <noormohammedb@protonmail.com>
2023-02-12 00:30:12 +0400

    cors error fixed
    temporarily fixed cors error on /api/login route
    a middleware handler for post
    a common use handler for options

---
+++ middlewares/utils.go
@@ -0,0 +1,19 @@
+package middlewares
+
+import "github.com/gin-gonic/gin"
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "content-type")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
